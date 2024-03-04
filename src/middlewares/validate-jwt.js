6ab75d01673fe6857098c7a1b4f7a29f27d46e4f
Informatica <informatica@alumno.local>
2024-03-04 16:24:33 -0600

    Updated the file middlewares

---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../user/user.model.js";
+
+export const validateJWT = async (req = response, res = response, next) => {
+    const token = req.header("x-token");
+
+    if (!token) {
+        return res.status(401).json({
+            msg: "Missing token"
+        });
+    }
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const user = await User.findById(uid);
+        if (!user) {
+            return res.status(401).json({
+                msg: "The user doesnt exist"
+            });
+        }
+        if (!user.status) {
+            return res.status(401).json({
+                msg: "Invalid token"
+            });
+        }
+        req.user = user;
+        next();
+    } catch (e) {
+        console.log(e);
+        res.stauts(401).json({
+            msf: "Invalid token"
+        });
+    }
+};
